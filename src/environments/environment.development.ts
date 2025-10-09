6bc4a4f25e63406f46b282eac913323e8378b282
Manthan Ankolekar <manthan.ank46@gmail.com>
2025-10-09 12:25:43 +0530

    Update environment files to switch API URL to production endpoint

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  // apiUrl: 'https://resume-ai-1w4m.onrender.com/api',
-  apiUrl: 'http://localhost:4000/api',
+  apiUrl: 'https://resume-ai-1w4m.onrender.com/api',
+  // apiUrl: 'http://localhost:4000/api',
   trackingApiUrl: 'https://visitor-tracking-api.vercel.app/api/visit',
 };
